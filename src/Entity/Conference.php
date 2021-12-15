7a1aee03a5432585a2c6a8292712b2866a513bf7
Maksym Alieksieienko <pzka@ukr.net>
2021-12-15 14:08:05 +0200

    add API composer req api

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,11 +2,21 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource; //add API
+use Symfony\Component\Serializer\Annotation\Groups; //add API
 use App\Repository\ConferenceRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
+ *
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="conference:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="conference:item"}}},
+ *     order={"year"="DESC", "city"="ASC"},
+ *     paginationEnabled=false
+ * )
  */
 class Conference
 {
@@ -15,21 +25,25 @@ class Conference
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $city;
 
     /**
      * @ORM\Column(type="string", length=4)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $year;
 
     /**
      * @ORM\Column(type="boolean")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $isInternational;
 
     public function __toString(): string
