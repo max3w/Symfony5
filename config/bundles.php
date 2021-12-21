20c269b97edd4ff9d5ed1d461890a30da4b6489a
Maksym Alieksieienko <pzka@ukr.net>
2021-12-21 15:17:46 +0200

    Revert "cont vhoda"
    This reverts commit 31a13f2ab144139b36050f0a3bedf46d9e841312.

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
