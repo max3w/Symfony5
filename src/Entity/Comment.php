c612c3e459090173414d63c106c235cd662b41cb
Maksym Alieksieienko <pzka@ukr.net>
2021-12-23 14:01:22 +0200

    Delete color
    delete color
    
    remove color API
    
    Test api product 1
    
    Update index.html.twig
    
    Update index.html.twig
    
    Update Specifications.php
    
    remove migration
    
    delete migrations
    
    delete mig
    
    del specifications & user
    
    Update DashboardController.php
    
    Revert "Update DashboardController.php"
    
    This reverts commit f5c2b5e4ca9db9ac1e6fc5612eb271b479bd4524.
    
    Revert "del specifications & user"
    
    This reverts commit ffdd925ebc07cc9bc23bcbabf44be092a963a8b4.
    
    Revert "delete mig"
    
    This reverts commit 8fc67c384679c591acdbf133f26a1574baf044ca.
    
    Revert "delete migrations"
    
    This reverts commit 4d487a1bfccce687416fcaf31429e12c5f54f458.
    
    Revert "remove migration"
    
    This reverts commit ece3957e9f2abb486a1686f9e8a93e4bf0567c06.
    
    Revert "Update Specifications.php"
    
    This reverts commit d8acbc0ff4dd555e14515e6ffa9c0f0dee88ac68.
    
    Revert "Update index.html.twig"
    
    This reverts commit 597a3d691cd46c4fc8e036eee3f4a77068586127.
    
    Revert "Update index.html.twig"
    
    This reverts commit 4c33c3ffd5aaf754984a97ef26150eaf74557049.
    
    Revert "remove color API"
    
    This reverts commit 1d92231bc6c430705cceb1816495832410636d41.
    
    Revert "delete color"
    
    This reverts commit 4748df73b0c243abd826e9930d8a4e13d3f3c6e6.
    
    install local
    
    Update Conference.php
    
    Update Conference.php
    
    Remove API conf&coments
    
    Update Comment.php
    
    Update Comment.php
    
    Revert "Revert "Update index.html.twig""
    
    This reverts commit 01e05d8826bec10b81c9ca1405e481b6e5b6c786.
    
    Revert "Revert "Update index.html.twig""
    
    This reverts commit 2531e0d539a0cdc3483447a07ecad901a7234a67.
    
    Revert "Revert "remove color API""
    
    This reverts commit 198a7af56b3dfd180d7431d5b2b4ddbbce26e089.

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,21 +2,16 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiFilter;
-use ApiPlatform\Core\Annotation\ApiResource;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+//use ApiPlatform\Core\Annotation\ApiFilter;
+//use ApiPlatform\Core\Annotation\ApiResource;
+//use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\CommentRepository;
-use Symfony\Component\Serializer\Annotation\Groups;
+//use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
  *
- * @ApiResource(
- * normalizationContext={"groups"={"my_comment:read"}}
- * )
- *
- * @ApiFilter(SearchFilter::class, properties={"conference": "exact"})
  */
 class Comment
 {
@@ -24,42 +19,36 @@ class Comment
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups({"my_coment:read"})
      */
 
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"my_coment:read"})
      */
 
     private $author;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"my_coment:read"})
      */
 
     private $text;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"my_coment:read"})
      */
 
     private $email;
 
     /**
      * @ORM\Column(type="datetime_immutable")
-     * @Groups({"my_comment:read"})
      */
 
     private $createdAt;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @Groups({"my_comment:read"})
      */
 
     private $photoFilename;
