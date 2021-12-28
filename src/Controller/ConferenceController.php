ab0597847bf46ed8a6ae18300ed393cc30cc4ef6
Maksym Alieksieienko <pzka@ukr.net>
2021-12-28 16:18:18 +0200

    add product twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository; //Добавляем для вывода товаров c БД
+use Twig\Environment; //главная точка входа TWIG
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +15,18 @@ class ConferenceController extends AbstractController
      * //@Route("/conference", name="conference")
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    /*public function index(): Response
     {
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
         
-    }
+    }*/
+    //Передаем все методы в TWIG
+    public function index(Environment $twig, ProductRepository $productRepository): Response
+     {
+        return new Response($twig->render('conference/index.html.twig', [
+            'product' => $productRepository->findAll(),
+        ]));
+     }
 }
