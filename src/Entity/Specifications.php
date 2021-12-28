3c1eb499255cca06fa63ba8a34a6e6a852282ce1
Maksym Alieksieienko <pzka@ukr.net>
2021-12-28 13:13:16 +0200

    add ignore

--- src/Entity/Specifications.php
+++ src/Entity/Specifications.php
@@ -6,9 +6,11 @@ use App\Repository\SpecificationsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 /**
  * @ORM\Entity(repositoryClass=SpecificationsRepository::class)
+ * @Ignore()
  */
 class Specifications
 {
