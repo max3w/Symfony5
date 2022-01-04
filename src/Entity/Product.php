9a9b97b872c6160ea93389f786d4d0f8bb50e364
Maksym Alieksieienko <pzka@ukr.net>
2022-01-05 00:16:14 +0200

    Update Product.php

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -10,9 +10,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
-#[ApiResource(api_keys: [
-    "X-Total-Count" => "10"
-])]
+#[ApiResource]()
 class Product
 {
     /**
