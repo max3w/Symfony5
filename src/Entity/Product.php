4ec0dfce98b540addcb7e1829157a11641029642
Maksym Alieksieienko <pzka@ukr.net>
2022-01-04 16:09:32 +0200

    add anotation groups

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
