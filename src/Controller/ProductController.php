21ad3c62240b3a139116d80a31a8eae5f636e17c
Maksym Alieksieienko <pzka@ukr.net>
2022-01-04 17:48:07 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -78,4 +78,8 @@ class ProductController extends AbstractController
 
         return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    $response = new JsonResponse($product);
+        $response->headers->set('X-Total-Count', "1");
+        return $response;
 }
